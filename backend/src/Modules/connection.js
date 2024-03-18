f9fdee84bce71bba11f39f4f40a8f1bae5e9ddd0
atishayarsh99 <121083187+atishayarsh99@users.noreply.github.com>
2024-03-18 17:39:36 +0530

    Update connection.js

--- backend/src/Modules/connection.js
+++ backend/src/Modules/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/test_db", {
+mongoose.connect("mongodb://testing:testing123@20.55.193.200:27017/admin", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
